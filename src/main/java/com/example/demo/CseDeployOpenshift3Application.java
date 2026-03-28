0fd9e65edec4125b9b869e77d0aeb6a862acd67d
Shalini-vetsa <shalinidivyavetsa@gmail.com>
2026-03-28 11:22:26 +0530

    first commit

---
+++ src/main/java/com/example/demo/CseDeployOpenshift3Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CseDeployOpenshift3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CseDeployOpenshift3Application.class, args);
+	}
+
+}
